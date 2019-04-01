fd07c9b9e3eaa80ccee3457a167bc88c9c2689c2
Shivam Patel <shivampatel066@gmail.com>
2019-04-01 10:29:20 +0530

    Random Fact Bank Template
    Random Fact Bank Template !!

--- FactBank/ViewController.swift
+++ FactBank/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func randomFact(_ sender: Any) {
+        
+        let array = ["""
+                     You burn more calories
+                     sleeping than you do
+                     watching television.
+                     """,
+                     """
+                     A single cloud can weight more
+                     than 1 million pounds.
+                     """,
+                     """
+                     The average person walks the
+                     equivalent of three times
+                     around the world in
+                     a lifetime.
+                     """,
+                     """
+                     If you believe that you’re truly
+                     one in a million, there are still
+                     approximately 7,184 more people
+                     out there just like you.
+                     """,
+                     """
+                     A sneeze travels about 100
+                     miles per hour.
+                     """,]
+        label.text = array.randomElement()
+        
+    }
+    
+    
 
 }
 
